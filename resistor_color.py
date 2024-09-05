b0ec9dae38b8c8deab6edafbc125603027e6fc40
Umair Abdullah <umaharabdullah@gmail.com>
2024-09-05 17:07:49 +0500

    2nd  commit

---
+++ resistor_color.py
@@ -0,0 +1,33 @@
+def color_code(color):
+    codes = {
+        "black": "0",
+        "brown": "1",
+        "red": "2",
+        "orange": "3",
+        "yellow": "4",
+        "green": "5",
+        "blue": "6",
+        "violet": "7",
+        "grey": "8",
+        "white": "9",
+    }
+    for key, value in codes.items():
+        if color == key:
+            return int(value)
+    return None
+
+
+def colors():
+    expected = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
+    return expected
